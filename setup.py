7994b1bcfd2c564c20ee1e7ecbdc827362b1d407
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-03-23 15:08:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This file is part of qdatamatrix.
+
+qdatamatrix is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+qdatamatrix is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with qdatamatrix.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from datamatrix import __version__
+from setuptools import setup, find_packages
+
+setup(
+	name=u'python-qdatamatrix',
+	version=__version__,
+	description= u'A PyQt4/PyQt5 widget for viewing and editing a DataMatrix object',
+	author=u'Sebastiaan Mathôt',
+	author_email=u's.mathot@cogsci.nl',
+	license=u'GNU GPL Version 3',
+	url=u'https://github.com/smathot/python-qdatamatrix',
+	packages=find_packages('.')
+)
