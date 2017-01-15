7981cab17701299aa4c8c441843ac28793f5dfa6
Sebastiaan Mathot <s.mathot@cogsci.nl>
2017-01-15 11:51:15 +0100

    Bump to 0.1.12

--- qdatamatrix/__init__.py
+++ qdatamatrix/__init__.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with qdatamatatrix.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = '0.1.10'
+__version__ = '0.1.12'
 
 from datamatrix.py3compat import *
 from qdatamatrix._qdatamatrix import QDataMatrix
